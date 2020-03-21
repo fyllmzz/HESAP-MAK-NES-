72158ce6ed5392702b4eb954b2635149ffe62952
fyllmzz <16008117049@ogr.bozok.edu.tr>
2020-03-21 23:54:23 +0300

    project pertama

---
+++ hesap makinesii/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesap_makinesii
+{
+	
+	public partial class Form1 : Form
+	{
+		bool optDurum = false;
+		double sonuc = 0;
+		string opt = "";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void RakamOlay(object sender, EventArgs e)
+		{
+			if (txtSonuc.Text == "0" || optDurum)
+				txtSonuc.Clear();
+
+			optDurum = false;
+			Button btn = (Button)sender;
+			txtSonuc.Text += btn.Text;
+
+		}
+
+		private void Opthesap(object sender, EventArgs e)
+		{
+			optDurum = true;
+			Button btn = (Button)sender;
+			string yeniOpt = btn.Text;
+
+			lblSonuc.Text = lblSonuc.Text + " " + txtSonuc.Text + " " + yeniOpt;
+			switch (opt)
+			{
+				case "+": txtSonuc.Text = (sonuc + Double.Parse(txtSonuc.Text)).ToString(); break;
+				case "-": txtSonuc.Text = (sonuc - Double.Parse(txtSonuc.Text)).ToString(); break;
+				case "*": txtSonuc.Text = (sonuc * Double.Parse(txtSonuc.Text)).ToString(); break;
+				case "/": txtSonuc.Text = (sonuc / Double.Parse(txtSonuc.Text)).ToString(); break;
+			}
+			sonuc = Double.Parse(txtSonuc.Text);
+			txtSonuc.Text = sonuc.ToString();
+			opt = yeniOpt;
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+
+
+
+			txtSonuc.Text = "0";
+
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+
+			txtSonuc.Text = "0";
+			lblSonuc.Text = "";
+			opt = "";
+			sonuc = 0;
+			optDurum = false;
+		}
+
+		private void button16_Click(object sender, EventArgs e)
+		{
+			lblSonuc.Text = "";
+			optDurum = true;
+			switch (opt)
+			{
+				case "+": txtSonuc.Text = (sonuc + Double.Parse(txtSonuc.Text)).ToString(); break;
+				case "-": txtSonuc.Text = (sonuc - Double.Parse(txtSonuc.Text)).ToString(); break;
+				case "*": txtSonuc.Text = (sonuc * Double.Parse(txtSonuc.Text)).ToString(); break;
+				case "/": txtSonuc.Text = (sonuc / Double.Parse(txtSonuc.Text)).ToString(); break;
+			}
+			sonuc = Double.Parse(txtSonuc.Text);
+			txtSonuc.Text = sonuc.ToString();
+			opt = "";
+
+		}
+
+		private void button18_Click(object sender, EventArgs e)
+		{
+			if (txtSonuc.Text == "0")
+			{
+				txtSonuc.Text = "0";
+			}
+			else if (optDurum)
+			{
+				txtSonuc.Text = "0";
+			}
+
+			if (!txtSonuc.Text.Contains(","))
+			{
+				txtSonuc.Text += ",";
+			}
+			optDurum = false;
+
+
+		}
+	}
+}
